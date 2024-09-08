fc8fdc75fdb34e7399733ffb7ac556aaaa193fd5
Akas Bagus Setiawan <146935384+AkasSakti@users.noreply.github.com>
2024-09-08 22:46:32 +0700

    Add files via upload

---
+++ ifelse1kond.php
@@ -0,0 +1,9 @@
+<?php
+$nilai = 80;
+
+if ($nilai >= 75) {
+    echo "Selamat, Anda lulus!";
+} else {
+    echo "Maaf, Anda tidak lulus.";
+}
+?>
---
+++ ifelsebnykkond.php
@@ -0,0 +1,13 @@
+<?php
+$nilai = 65;
+
+if ($nilai >= 85) {
+    echo "Nilai Anda: A";
+} elseif ($nilai >= 75) {
+    echo "Nilai Anda: B";
+} elseif ($nilai >= 65) {
+    echo "Nilai Anda: C";
+} else {
+    echo "Nilai Anda: D";
+}
+?>
